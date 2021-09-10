8bd301ab75fa612f4c1ec05c731a5a7a1d0035b7
mondala2001 <90471610+mondala2001@users.noreply.github.com>
2021-09-10 17:48:05 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "myfunc.h"
+
+int main()
+{
+	std::cout<<myfunc()<<std::endl;
+	return 0;
+}
---
+++ myfunc.cpp
@@ -0,0 +1,8 @@
+#include "myfunc.h"
+
+std::string myfunc()
+{
+	//a different comment
+	return "hello";
+}
+
